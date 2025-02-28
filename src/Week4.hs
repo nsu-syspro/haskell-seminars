17e99ec4a23f42fa9f925bd296de0dd6f607e564
Ivan Trepakov <liontiger23@gmail.com>
2025-02-28 12:29:50 +0700

    Update Week4

--- src/Week4.hs
+++ src/Week4.hs
@@ -1,4 +1,7 @@
-import Prelude hiding (product)
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE BangPatterns #-}
+
+import Prelude hiding (product, reverse, (++))
 
 -- fst :: (a, b) -> a
 
@@ -27,6 +30,10 @@ f4 f = \(x, y) -> f x y
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' f xs ys = map (uncurry f) $ zip xs ys
 
+zipWith'' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith'' f = (map (uncurry f) .) . zip
+
+
 
 -- ($) -- application
 --f5 :: (a -> b) -> (a -> b)
@@ -50,7 +57,7 @@ sumEvenSquares = sum . filter even . map (^2)
 
 sumEvenSquares' :: [Int] -> Int
 sumEvenSquares' =
-  let 
+  let
     square :: Int -> Int
     square x = x * x
 
@@ -88,5 +95,91 @@ product xs = foldr (*) 1 xs
 data LRList a = Nil | a :< LRList a | LRList a :> a
   deriving (Show)
 
+-----------------
+-- Day 2
+-----------------
+
+(++) :: [a] -> [a] -> [a]
+[]       ++ ys = ys
+(x : xs) ++ ys = x : (xs ++ ys)
+
+reverse :: [a] -> [a]
+reverse [] = []
+reverse (x : xs) = reverse xs ++ [x]
+
+--   reverse (0 : 1 : 2 : [])
+-- ~ reverse (1 : 2 : []) ++ [0]
+-- ~ (reverse (2 : []) ++ [1]) ++ [0]
+-- ~ ((reverse [] ++ [2]) ++ [1]) ++ [0]
+-- ~ (([] ++ [2]) ++ [1]) ++ [0]
+-- ~  ([2] ++ [1]) ++ [0]
+-- ~  ((2 : []) ++ [1]) ++ [0]
+-- ~   (2 : ([] ++ [1])) ++ [0]
+
+reverseAcc :: [a] -> [a]
+reverseAcc = go []
+ where
+  go :: [a] -> [a] -> [a]
+  go !acc []       = acc
+  go !acc (x : xs) = go (x : acc) xs
 
 
+sumSlow :: Num a => [a] -> a
+sumSlow [] = 0
+sumSlow (x : xs) = x + sumSlow xs
+
+sumAcc :: Num a => [a] -> a
+sumAcc = go 0
+ where
+  go acc [] = acc
+  go acc (x : xs) = go (x + acc) xs
+
+
+--   sumAcc (1 : 2 : 3 : [])
+-- ~ go 0 (1 : 2 : 3 : [])
+-- ~ go (0 + 1) (2 : 3 : [])
+-- ~ go ((0 + 1) + 2) (3 : [])
+-- ~ go (((0 + 1) + 2) + 3) []
+-- ~ (((0 + 1) + 2) + 3)
+-- ~ ((1 + 2) + 3)
+-- ~ (3 + 3)
+-- ~ 6
+
+
+sumAcc' :: Num a => [a] -> a
+sumAcc' = go 0
+ where
+  go !acc [] = acc
+  go !acc (x : xs) = go (x + acc) xs
+
+-- Bang pattern
+--
+-- Enabled by default since GHC2021
+-- Before needs to be enabled with pragma
+--
+-- {-# LANGUAGE BangPatterns #-}
+--
+-- Forces not full evaluation, but only to WHNF
+-- (Weak Head Normal Form)
+
+sumAcc'' :: Num a => [a] -> a
+sumAcc'' = go 0
+ where
+  go acc [] = acc
+  go acc (x : xs) =
+    let acc' = x + acc
+    in  acc' `seq` go acc' xs
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' f = go
+ where
+  go !acc [] = acc
+  go !acc (x : xs) = go (f acc x) xs
+
+sumAcc''' :: Num a => [a] -> a
+sumAcc''' = foldl' (+) 0
+
+-- Useful materials:
+--
+--   https://tech.fpcomplete.com/haskell/tutorial/all-about-strictness/
+--   https://wiki.haskell.org/Foldr_Foldl_Foldl'
