5d2667eb83eabc1c7852ea6834ee1c99cc4724e0
Ivan Trepakov <liontiger23@gmail.com>
2025-04-04 12:28:46 +0700

    Start Functor

---
+++ src/Functor.hs
@@ -0,0 +1,10 @@
+
+-- We will define Functor later
+
+-- Laws
+--
+--   (1) fmap id = id
+--   (2) fmap f . fmap g = fmap (f . g)
+--
+-- Moreover, in Haskell (2) can be derived from (1)
+-- Due to "parametricity"
