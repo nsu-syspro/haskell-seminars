708dab2f7e7d7f8f63e43e94e8c165f69399c491
Ivan Trepakov <liontiger23@gmail.com>
2025-05-02 12:28:13 +0700

    Update Traversable Laws

--- src/Traversable.hs
+++ src/Traversable.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Prelude hiding (Traversable(..))
+import Control.Applicative (Applicative(liftA2))
 import Data.Coerce (coerce)
 import Text.Read (readMaybe)
 
@@ -147,3 +148,87 @@ plusTen s = case readMaybe @Double s of
   Just v -> Just $ show (v + 10)
   Nothing -> Nothing
 
+-- * Laws
+--
+-- - Naturality
+--
+--     t . traverse f = traverse (t . f)
+--
+-- - Identity
+--
+--     traverse Identity = Identity
+--
+-- - Composition
+--
+--     traverse (Compose . fmap g . f) = Compose . fmap (traverse g) . traverse f
+--
+-- Signatures:
+--
+--   (Traverse t, Applicative u, Applicative w) => 
+--
+--     traverse :: (a -> u b) -> t a -> u (t b)
+--     f :: a -> u b
+--
+--     g :: b -> w c
+--
+--     fmap g . f :: a -> u (w c)
+--
+--     Applicative transformation from functor u to w
+--     t :: u b -> w c
+--
+--     Compose :: u (w a) -> Compose u w a
+--
+--     We can guess that Compose data type is like this:
+--
+--       newtype Compose u w a = Compose (u (w a))
+--
+--     Like composition on functions:
+--
+--       (f . g) x = f (g x)
+--     
+--     traverse f :: t a -> u (t b)
+--     fmap (traverse g) :: u (t b) -> u (w (t c))
+--     fmap (traverse g) . traverse f :: t a -> u (w (t c))
+
+
+newtype Compose f g a = Compose { getCompose :: f (g a) }
+  deriving (Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap :: (a -> b) -> Compose f g a -> Compose f g b
+  --fmap = coerce (fmap @f @(g a) @(g b) . fmap @g)
+  --fmap f (Compose fga) = Compose (fmap (fmap f) fga)
+  fmap f = Compose . fmap (fmap f) . getCompose
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure = Compose . pure . pure
+
+  liftA2 :: (a -> b -> c) -> Compose f g a -> Compose f g b -> Compose f g c
+  liftA2 f (Compose fga) (Compose fgb) = Compose ((liftA2 . liftA2) f fga fgb)
+
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  Compose fga2b <*> Compose fga = Compose ((<*>) <$> fga2b <*> fga)
+  -- (<*>) :: g (a -> b) -> (g a -> g b)
+  -- fmap :: (a -> b) -> (f a -> f b)
+  -- fmap (<*>) :: f (g (a -> b)) -> f (g a -> g b)
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap :: Monoid m => (a -> m) -> Compose f g a -> m
+  --foldMap f (Compose fga) = foldMap (foldMap f) fga
+  foldMap f = foldMap (foldMap f) . getCompose
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+  traverse :: Applicative u => (a -> u b) -> Compose f g a -> u (Compose f g b)
+  --traverse f (Compose fga) = Compose <$> traverse (traverse f) fga
+  traverse f = fmap Compose . traverse (traverse f) . getCompose
+
+
+-- Task'ish
+--
+-- How can we turn Tree a -> Tree Int
+-- so that it numerates each value 1, 2....
+--
+-- >>> numerate exampleTree
+-- Branch (Leaf 1) 2 (Branch (Leaf 3) 4 Empty)
+--
