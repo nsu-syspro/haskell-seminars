9c75b57d47b4810af028236b9aa1a2bb9b8d6409
Ivan Trepakov <liontiger23@gmail.com>
2026-03-11 10:32:07 +0700

    week05: update quiz

--- Week05/Quiz08.hs
+++ Week05/Quiz08.hs
@@ -1,9 +1,13 @@
 import Data.Function (on)
+import Data.Ord
 
-g1, g2, g3 :: Ord a => (b -> a) -> (b -> b -> Ordering)
+g1, g2, g3, g4 :: Ord a => (b -> a) -> (b -> b -> Ordering)
 g1 f x y = compare (f x) (f y)
 g2 = (compare `on`)
 g3 f = compare `on` f
+g4 = comparing
 
 -- ghci> sortBy (compare `on` snd) [(1, "foo"), (2, "bar")]
+-- [(2,"bar"),(1,"foo")]
+-- ghci> sortBy (comparing snd) [(1, "foo"), (2, "bar")]
 -- [(2,"bar"),(1,"foo")]
