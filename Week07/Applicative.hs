87b1e108fa47555a6e93973b4f16a92a77ced347
Ivan Trepakov <liontiger23@gmail.com>
2026-03-27 10:37:52 +0700

    week07: udpate applicative

--- Week07/Applicative.hs
+++ Week07/Applicative.hs
@@ -14,17 +14,37 @@ class Functor f => Applicative f where
   liftA2 g fa fb = g <$> fa <*> fb
 
   (*>) :: f a -> f b -> f b
-  _ *> fb = fb
+  (*>) = liftA2 (\_ b -> b)
+  --fa *> fb = liftA2 (\_ b -> b) fa fb
+  --fa *> fb = (\_ b -> b) <$> fa <*> fb
 
-  --(<*) :: f a -> f b -> f a
+  (<*) :: f a -> f b -> f a
+  --fa <* fb = liftA2 const fa fb
+  (<*) = liftA2 const
 
 liftA3 :: Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 liftA3 g fa fb fc = g <$> fa <*> fb <*> fc
+
+liftA :: Applicative f => (a -> b) -> f a -> f b
+liftA f fa = pure f <*> fa
 
 instance Applicative Maybe where
   pure = Just
   Just g <*> Just x = Just (g x)
   _ <*> _ = Nothing
+
+instance Applicative [] where
+  fs <*> xs = [ f x | f <- fs, x <- xs]
+  pure x = [x]
+
+newtype ZipList a = ZipList { getZipList :: [a] }
+
+instance Functor ZipList where
+  fmap = liftA
+
+instance Applicative ZipList where
+  ZipList fs <*> ZipList xs = ZipList $ zipWith ($) fs xs
+  pure x = ZipList (repeat x)
 
 data Student = Student String Int
 
@@ -36,6 +56,9 @@ mkStudent' name group = Student <$> name <*> pure group
 
 mkStudent'' :: String -> Maybe Int -> Maybe Student
 mkStudent'' name group = Student name <$> group
+
+--data Tree a = Empty | Leaf a | Branch (Tree a) a (Tree a)
+
 
 
 
--- Week07/Quiz11.hs
+++ Week07/Quiz11.hs
@@ -1,5 +1,6 @@
-import Data.Monoid (First(..))
+import Data.Monoid (First(..), Endo(..))
 import Data.Foldable
+import Data.Ord
 
 find :: Foldable t => (a -> Bool) -> t a -> Maybe a
 find p = getFirst . foldMap (First . toMaybe p)
@@ -23,7 +24,26 @@ instance Ord a => Semigroup (MaxMaybe a) where
 instance Ord a => Monoid (MaxMaybe a) where
   mempty = MaxMaybe Nothing
 
---safeMaximumBy :: Foldable t => (a -> a -> Ordering) -> t a -> Maybe a
+-- >>> safeMaximumBy compare [1,2,3]
+-- Just 3
+-- >>> safeMaximumBy compare [3,2,1]
+-- Just 3
+-- >>> safeMaximumBy compare []
+-- Nothing
+-- >>> safeMaximumBy (comparing length) ["abc", "abcd", "ab"]
+-- Just "abcd"
+-- >>> safeMaximumBy (comparing (Down . length)) ["abc", "abcd", "ab"]
+-- Just "ab"
+
+safeMaximumBy :: forall t a. Foldable t => (a -> a -> Ordering) -> t a -> Maybe a
 --safeMaximumBy cmp = _ . foldMap _
 --safeMaximumBy cmp = foldl' (_) Nothing
+safeMaximumBy cmp xs = appEndo (foldMap (Endo . maybeMax . Just) xs) Nothing
+  where
+    maybeMax :: Maybe a -> (Maybe a -> Maybe a)
+    maybeMax (Just x) (Just y)
+      | cmp x y == GT = Just x
+      | otherwise     = Just y
+    maybeMax Nothing y = y
+    maybeMax x Nothing = x
 
